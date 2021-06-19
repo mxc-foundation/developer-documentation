4a08e4a5b86ff56bef25d28be5178fab5e1d6439
Jeff Stahlnecker <45363541+jeffstahlnecker@users.noreply.github.com>
2021-06-19 20:36:10 +0200

    ran prettier, updated doc/blog edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,6 @@ module.exports = {
       // Should we use the prefers-color-scheme media-query,
       // using user system preferences, instead of the hardcoded defaultMode
       respectPrefersColorScheme: true,
-
     },
     navbar: {
       title: 'MXC Developer Documentation',
@@ -41,7 +40,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/mxc-foundation/developer-documentation',
           label: 'GitHub',
           position: 'right',
         },
@@ -85,7 +84,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/mxc-foundation/',
             },
           ],
         },
@@ -105,13 +104,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/mxc-foundation/developer-documentation/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/mxc-foundation/developer-documentation/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -30,7 +31,8 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
